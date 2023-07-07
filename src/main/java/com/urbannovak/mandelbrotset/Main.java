43be05a77580ac6db15154478cea3ccd6cb77641
UNovak <urbanwritescode@gmail.com>
2023-07-08 00:24:57 +0200

    Created the first window

--- src/main/java/com/urbannovak/mandelbrotset/Main.java
+++ src/main/java/com/urbannovak/mandelbrotset/Main.java
@@ -1,20 +1,78 @@
 package com.urbannovak.mandelbrotset;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
+
+
+    Stage window;
+    Scene menu;
+    Scene image;
+
+    RadioButton sequential;
+    RadioButton parallel;
+    RadioButton MPI;
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
-        stage.setScene(scene);
-        stage.show();
+        window = stage;
+        window.setTitle("Mandelbrot Set");
+        window.setResizable(false);
+
+        VBox vbox = new VBox(10);
+        Scene menu = new Scene(vbox,250,250);
+        vbox.setAlignment(Pos.CENTER);
+
+        Label displayTxt = new Label("Display image?");
+        ToggleButton displayImage = new ToggleButton("NO");
+        displayImage.setSelected(false);
+        displayImage.setOnAction(e -> {
+            if (displayImage.isSelected()) displayImage.setText("YES");
+            else displayImage.setText("NO");
+        });
+
+        HBox hbox = new HBox(5);
+        hbox.setAlignment(Pos.CENTER);
+        Label modeTxt = new Label("How should the program be run");
+        ToggleGroup modeGroup = new ToggleGroup();
+
+        sequential = new RadioButton("sequential");
+        sequential.getStyleClass().remove("radio-button");
+        sequential.getStyleClass().add("toggle-button");
+        sequential.setSelected(true);
+
+        parallel = new RadioButton("parallel");
+        parallel.getStyleClass().remove("radio-button");
+        parallel.getStyleClass().add("toggle-button");
+
+        MPI = new RadioButton("MPI");
+        MPI.getStyleClass().remove("radio-button");
+        MPI.getStyleClass().add("toggle-button");
+
+        MPI.setToggleGroup(modeGroup);
+        parallel.setToggleGroup(modeGroup);
+        sequential.setToggleGroup(modeGroup);
+
+        hbox.getChildren().addAll(sequential,parallel,MPI);
+
+        SplitPane br = new SplitPane();
+
+        Button start = new Button("START");
+        start.setMinSize(80,40);
+
+
+        vbox.getChildren().addAll(displayTxt,displayImage,modeTxt,hbox,br,start);
+
+        window.setScene(menu);
+        window.show();
     }
 
     public static void main(String[] args) {
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,8 +1,8 @@
 module com.urbannovak.mandelbrotset {
     requires javafx.controls;
     requires javafx.fxml;
-            
-                            
+
+
     opens com.urbannovak.mandelbrotset to javafx.fxml;
     exports com.urbannovak.mandelbrotset;
 }
